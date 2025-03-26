f2083f96e4926bcfa29d03354dece326a718a7cf
Jesús Díaz <diazmontejo125@gmail.com>
2025-03-25 18:00:57 -0600

    ActividadesAvance

--- src/context/ActividadContext/ActividadContext.tsx
+++ src/context/ActividadContext/ActividadContext.tsx
@@ -1,5 +1,5 @@
-import Reac, {createContext} from 'react'
-
+import Reac, {createContext, useState} from 'react'
+import { ActModel } from '../../models';
 import {ActividadContextType} from './ActividadContext.type'
 
 export const ActividadContext = createContext<ActividadContextType.Context>({
@@ -10,5 +10,30 @@ export const ActividadContext = createContext<ActividadContextType.Context>({
     abrirFormActi: () => {},
     crearActividad: () => {},
     checkCompleto: () => {},
-
 })
+
+export function ActividadesProvider(props:ActividadContextType.Props){
+    const {children} = props;
+
+    const [totalAct, setTotalAct] = useState(8);
+    const [totalCompActi, setTotalCmpActi] = useState(5);
+    const [actividades, seActividades] = useState<ActModel[]>([]);
+    const [completActi, setComletActi] = useState<ActModel[]>([]);
+
+    const valueContext:ActividadContextType.Context = {
+        totalAct,
+        totalCompActi,
+        actividades: [],
+        completActi: [],
+        abrirFormActi: () => {},
+        crearActividad: () => {},
+        checkCompleto: () => {},
+
+    }
+
+    return(
+        <ActividadContext.Provider value={valueContext}>
+            {children}
+        </ActividadContext.Provider>
+    )
+}
